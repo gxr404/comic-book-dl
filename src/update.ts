04b3e86abeff01e05b8ca703c9270f29c9591bff
gxr404 <3356209713@qq.com>
2024-01-13 16:58:27 +0800

    fix: 更新时错误仅打印书名不打印具体错误的图片

--- src/update.ts
+++ src/update.ts
@@ -1,10 +1,9 @@
 import {resolve} from 'node:path'
 import { confirm, checkbox } from '@inquirer/prompts'
-import { run } from '@/core'
-import { echoErrorMsg } from '@/index'
+import { ErrorChapterItem, run } from '@/core'
 import { logger, notEmpty } from '@/utils'
 import { scanFolder } from '@/lib/download'
-import type { BookInfo } from '@/lib/parse/base'
+import type { BookInfo, ChaptersItem } from '@/lib/parse/base'
 
 interface Config {
   bookPath: string,
@@ -70,4 +69,36 @@ async function updateRun(bookInfo: BookInfo, bookDistPath: string) {
       })
     }
   })
+}
+
+interface ChapterErrorMsgItem {
+  chapterName: string,
+  imgList: string[]
+}
+
+function echoErrorMsg(
+  bookName: string,
+  chaptersList: ChaptersItem[],
+  errorList: ErrorChapterItem[]
+) {
+  const errChaptersMsg: ChapterErrorMsgItem[] = []
+  errorList.forEach((item)=>{
+    const errChapter = errChaptersMsg.find(msg => {
+      return msg.chapterName === item.chapter.name
+    })
+    if (errChapter && item.imgUrl) {
+      errChapter.imgList.push(item.imgUrl)
+    } else {
+      errChaptersMsg.push({
+        chapterName: item.chapter.name,
+        imgList: item.imgUrl ? [item.imgUrl] : []
+      })
+    }
+  })
+
+  logger.error(`《${bookName}》本次执行总数${chaptersList.length}话，✕ 失败${errChaptersMsg.length}话`)
+  for (const errInfo of errChaptersMsg) {
+    logger.error(`  └── ✕ ${errInfo.chapterName}`)
+  }
+  logger.error('o(╥﹏╥)o 由于网络波动或链接失效以上下载失败，可重新执行命令重试(PS:不会影响已下载成功的数据)')
 }
